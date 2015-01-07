f3d23ba7f2c8802c9a2d5e04e2a8f0121dc7bb29
jc <jc@vgame.com.tw>
2015-01-07 10:36:43 +0800

    add main test

---
+++ Assets/Script/Editor/UnitTest/MainTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+using UnityEngine;
+using System.Linq;
+
+namespace VGame.UnitTest
+{
+    [TestFixture]
+    public class MainTests
+    {
+        [UnityEditor.MenuItem("VGame/Test")]
+        public static void Run()
+        {
+            UnityTest.Batch.RunUnitTests();
+            
+        }
+
+
+        [Test]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
