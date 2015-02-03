f4b137a23a530bdab2ea16290f16b941ddccf29b
jc <jc@vgame.com.tw>
2015-02-03 14:19:58 +0800

    change path

--- Assets/clone_from_remoting.cmd
+++ Assets/clone_from_remoting.cmd
@@ -1,10 +1,10 @@
-del Assets\Plugins\Regulus\*.* /s /q 
+del Assets\FishHunter\Plugins\Regulus\*.* /s /q 
 
-copy ..\..\Regulus\Projects\FishHunter\Common\bin\Debug\*.dll Assets\Plugins\Regulus
-copy ..\..\Regulus\Projects\FishHunter\User\bin\Debug\*.dll Assets\Plugins\Regulus
+copy ..\..\Regulus\Projects\FishHunter\Common\bin\Debug\*.dll Assets\FishHunter\Plugins\Regulus
+copy ..\..\Regulus\Projects\FishHunter\User\bin\Debug\*.dll Assets\FishHunter\Plugins\Regulus
 
 
-del Assets\RemotingCode\*.* /s /q
-copy ..\..\Regulus\Projects\FishHunter\Game\*.cs Assets\RemotingCode
+del Assets\FishHunter\RemotingCode\*.* /s /q
+copy ..\..\Regulus\Projects\FishHunter\Game\*.cs Assets\FishHunter\RemotingCode
 
 pause
