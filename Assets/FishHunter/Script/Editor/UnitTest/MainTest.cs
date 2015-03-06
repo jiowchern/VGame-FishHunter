b6b5dba6207d2b8513e894fd99ed9fca8f4999ee
jc <jc@vgame.com.tw>
2015-03-06 09:54:14 +0800

    remove unity test tools

--- Assets/FishHunter/Script/Editor/UnitTest/MainTest.cs
+++ Assets/FishHunter/Script/Editor/UnitTest/MainTest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Threading;
 using NUnit.Framework;
@@ -23,4 +23,4 @@ namespace VGame
             //Assert.Fail();
         }
     }
-}
+}*/
