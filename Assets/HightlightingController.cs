47bcb95b993a8d355139c7fc1000d587d2bff02d
terry <terry.chen19821016@gmail.com>
2015-03-12 17:05:41 +0800

    Highlight Plugin Import

---
+++ Assets/HightlightingController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using HighlightingSystem;
+
+
+public class HightlightingController : MonoBehaviour 
+{
+    //HighlighterController
+    protected Highlighter h;
+
+    // 
+    protected void Awake()
+    {
+        h = GetComponent<Highlighter>();
+        //if (h == null) { h = gameObject.AddComponent<Highlighter>(); }
+        
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        h.ConstantOn(Color.yellow);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+}
