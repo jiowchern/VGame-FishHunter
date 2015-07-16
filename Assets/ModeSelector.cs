e23bb2cc46256a5e7d296b8393ed8081aa3a0a39
jc <jc@vgame.com.tw>
2015-07-16 14:29:53 +0800

    add select connect mode

---
+++ Assets/ModeSelector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModeSelector : MonoBehaviour {
+
+
+    public Client Target;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void RunRemoting()
+    {
+        Target.Mode = Client.MODE.REMOTING;
+        Target.Initial();
+    }
+
+    public void RunStandalong()
+    {
+        Target.Mode = Client.MODE.STANDALONG;
+        Target.Initial();
+    }
+}
